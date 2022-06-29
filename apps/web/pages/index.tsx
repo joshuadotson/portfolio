324d19e2dd1ecf6e5047977061a7bab6ead42b19
Dotson, Josh <Josh.Dotson@sabre.com>
2022-06-29 10:50:50 -0400

    testing

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,8 @@ import { Button } from "ui";
 export default function Web() {
   return (
     <div>
-     <h1>Coming soon... a portfolio!!!</h1>
+     <h1>Joshua Dotson</h1>
+     <h2>A portfolio...</h2>
     </div>
   );
 }
